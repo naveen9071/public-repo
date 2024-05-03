fb8afa3e844583c9f25fa163811ce7d319c7d597
Naveenkumar K <121014694+naveen9071@users.noreply.github.com>
2024-05-03 11:48:20 +0530

    variable.tf

---
+++ variable.tf
@@ -0,0 +1,33 @@
+#variable "count" {
+#  default = "1"
+#}
+variable "ami" {
+  default = "ami-042b34111b1289ccd"
+}
+variable "instance_type" {
+  default = "t2.medium"
+}
+variable "Name" {
+  default = "Terraform-k8s"
+}
+variable "key_name" {
+  default = "open-terraform"
+}
+variable "security_group_id" {
+  default = "sg-0c2fe03036ede5b16"
+}
+variable "subnet_id" {
+  default = "subnet-0c1f151a45b855597"
+}
+variable "public_ip" {
+  default = "true"
+}
+variable "root_volume_size" {
+  default = "20"
+}
+variable "ebs_device_name" {
+  default = "/dev/xvda"
+}
+variable "ebs_volume_size" {
+  default = "30"
+}
